ab008bec234d935c0f3ea00a12a59ea8d44adc94
ClaudiaSolomon <claudia.solomon39@yahoo.com>
2024-03-11 19:45:35 +0200

    Create lab2.dart

---
+++ lab2.dart
@@ -0,0 +1,135 @@
+//1
+void largeNumber(List<String> args) {
+  var numbers = <int>[];
+
+  if (args.isEmpty) {
+    print('no arguments given');
+    return;
+  }
+
+  for (int i = 0; i < args.length; i++) {
+    if (int.tryParse(args[i]) != null) {
+      numbers.add(int.parse(args[i]));
+    }
+  }
+  var ok=0;
+  int i=numbers.length-1;
+  while(numbers[i]==9){
+    ok=1;
+    numbers[i]=0;
+    i--;
+    if(i==-1)
+    {
+      break;
+    }
+  }
+  if(i==-1)
+  {
+    if(ok==1){
+    numbers[0]=1;
+    numbers.add(0);
+  }
+  }
+  else{
+    numbers[i]++;
+  }
+  print(numbers);
+}
+
+//2
+void cuvant(List<String> args)
+{
+  var map = Map<String,int>();
+  if (args.isEmpty) {
+    print('no arguments given');
+    return;
+  }
+
+  for (int i = 0; i < args.length-1; i++) {
+    map[args[i]]=int.parse(args[i+1]);
+    i++;
+  }
+  String s=args[args.length-1];
+  int suma=0;
+  for(int i=0;i<s.length;i++)
+  {
+    suma+=map[s[i]]??0;
+  }
+  print(suma);
+}
+
+//3
+int perechi_bune(List<int> l)
+{
+  int nr=0;
+  var s = Set<int>(); 
+  for(int i=0;i<l.length;i++)
+  {
+    for(int j=i+1;j<l.length;j++)
+    {
+      if(l[i]==l[j]){
+        s.add(l[i]);
+        nr++;
+      }
+    }
+  }
+  print(s);
+  return nr;
+}
+
+//4
+int grupuri(int n)
+{
+  int nr=0;
+  int m=n;
+  int suma=0;
+  while(m!=0)
+  {
+    suma+=m%10;
+    m=m~/10;
+  }
+  List<int> l = List<int>.filled(999, 0,growable: true);
+  
+  for(int j=0;j<=n;j++)
+  {
+    suma=0;
+    int i=j;
+    while(i!=0)
+    {
+      suma+=i%10;
+      i=i~/10;
+    }
+    if (l.length <= suma) {
+      l.length = suma + 1;
+    }
+    l[suma]++;
+  }
+  int maxi=0;
+  for(int i=0;i<l.length;i++)
+  {
+    if(l[i]>maxi)
+    {
+      maxi=l[i];
+    }
+  }
+  for(int i=0;i<l.length;i++)
+  {
+    if(l[i]==maxi)
+    {
+      nr++;
+    }
+  }
+  return nr;
+}
+
+void main(List<String> args) {
+  //largeNumber(args);
+
+  //cuvant(args);
+
+  // int nr=perechi_bune([1,2,3,4,1,2,6,2,7]);
+  // print("nr perechi=$nr");
+
+  int nr=grupuri(30);
+  print("nr grupuri=$nr");
+}
